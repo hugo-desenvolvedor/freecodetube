2c765c2c4d7e824e89af07cc1af071d56b71afd2
Hugo Santos <hugo.desenvolvedor@gmail.com>
2021-03-17 14:33:25 -0300

    Add backend nested layout

--- backend/views/layouts/auth.php
+++ backend/views/layouts/auth.php
@@ -10,12 +10,10 @@ use common\widgets\Alert;
 AppAsset::register($this);
 $this->beginContent('@backend/views/layouts/base.php');
 ?>
-    <main class="d-flex">
-        <?= $this->render("_sidebar") ?>
-
-        <div class="content-wrapper p-3">
-            <?= Alert::widget() ?>
-            <?= $content ?>
-        </div>
-    </main>
+<main class="d-flex">
+    <div class="content-wrapper p-3">
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</main>
 <?php $this->endContent(); ?>
